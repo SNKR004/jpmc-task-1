f6160933b68ed6c960cf178dab6ec04343c9f14c
Sankar Addala <95013212+SNKR004@users.noreply.github.com>
2023-03-16 15:36:32 +0530

    Update client.py

--- client.py
+++ client.py
@@ -25,7 +25,9 @@ if __name__ == "__main__":
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-        for i in quotes:
-            stock, bid_price, ask_price, price = getDataPoint(i)
-            print(f"Quoted {stock} at (bid:{bid_price}, ask:{ask_price}, price:{price})")
-        print(f"Ratio {getRatio(price['ABC'],price['DEF'])}")
+        prices = {}
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+        print("Ratio %s" % getRatio(prices["ABC"], prices["DEF"]))
